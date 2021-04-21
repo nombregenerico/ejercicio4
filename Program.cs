0d03e45c495765f19b8c47104e2486c195c6270c
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2021-04-21 01:57:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio4
+{
+    public sealed class ExpresionLambdaComoParametro
+    {
+        // Método que se encarga de operar los operandos 
+        // de una operación binaria (e.g., suma, resta, producto, cociente):
+        public static int Calcular(Func<int, int, int> operacion, int num1, int num2)
+        {
+            return operacion(num1, num2);
+        }
+
+        public static void Main()
+        {
+            Console.Write("Escriba un número entero: ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Escribe otro número entero: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, Calcular((x, y) => x + y, num1, num2));
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, Calcular((x, y) => x - y, num1, num2));
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, Calcular((x, y) => x * y, num1, num2));
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, Calcular((x, y) => x / y, num1, num2));
+        }
+    }
+}
